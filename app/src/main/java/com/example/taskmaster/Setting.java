153425b10b47dd96e7a0b5054f3beec83bfb1b5b
shathasmadi <alsmadishatha@gmail.com>
2021-08-30 16:31:47 +0300

    lab 27

---
+++ app/src/main/java/com/example/taskmaster/Setting.java
@@ -0,0 +1,32 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        Button save = findViewById(R.id.saveButton);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedData = PreferenceManager.getDefaultSharedPreferences(Setting.this);
+                SharedPreferences.Editor sharedEditor = sharedData.edit();
+                EditText userName = findViewById(R.id.editText);
+                String name = userName.getText().toString();
+                sharedEditor.putString("username",name);
+                sharedEditor.apply();
+            }
+        });
+
+    }
+}
