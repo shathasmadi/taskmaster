04f1612d610fb98e32a5e3298c3c66a30f521c62
shathasmadi <alsmadishatha@gmail.com>
2021-09-05 16:57:36 +0300

    lab31

---
+++ app/src/androidTest/java/com/example/taskmaster/AddTaskTest.java
@@ -0,0 +1,38 @@
+package com.example.taskmaster;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class AddTaskTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity>main =
+            new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void addTaskTest(){
+
+    onView(withId(R.id.but)).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.taskTitleView)).check(matches(isDisplayed())).perform(typeText("Espresso Task"));
+        onView(withId(R.id.taskDescriptionView)).check(matches(isDisplayed())).perform(typeText("Espresso Description"));
+        onView(withId(R.id.taskStateView)).check(matches(isDisplayed())).perform(typeText("Espresso State"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.button)).check(matches(isDisplayed())).perform(click());
+        onView(withText("Espresso Task")).check(matches(isDisplayed()));
+
+    }
+}
--- app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
@@ -1,14 +1,27 @@
 package com.example.taskmaster;
 
-import android.content.Context;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
 
+import android.content.Context;
+import android.support.test.espresso.action.ViewActions;
+
+import androidx.test.espresso.ViewAction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +30,37 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    public static final String STRING_TO_BE_TYPED = "Espresso";
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.taskmaster", appContext.getPackageName());
     }
-}
+
+    @Rule
+    public ActivityScenarioRule<AllTask> All =
+            new ActivityScenarioRule<>(AllTask.class);
+    @Test
+    public void headingIsVisible() {
+        onView(withText("All Tasks")).check(matches(isDisplayed()));
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> main =
+            new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void testMainActivityButtons() {
+        onView(withId(R.id.settingButton)).perform(click());
+
+
+    }
+//    @Test
+//    public void taskDetailsTest(){
+//        onView(withId(R.id.fragmentButton)).check(matches(isDisplayed())).perform(click());
+//        onView(withText("Reading Task")).check(matches(isDisplayed()));
+//
+//    }
+
+   }
+
