41ccfd6f67327dda65bff65d881c8d1a33608210
shathasmadi <alsmadishatha@gmail.com>
2021-09-07 23:50:37 +0300

    lab 32

---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/TaskMaster.java
@@ -0,0 +1,228 @@
+package com.amplifyframework.datastore.generated.model;
+
+import com.amplifyframework.core.model.temporal.Temporal;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Objects;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
+
+/** This is an auto generated class representing the TaskMaster type in your schema. */
+@SuppressWarnings("all")
+@ModelConfig(pluralName = "TaskMasters")
+public final class TaskMaster implements Model {
+  public static final QueryField ID = field("TaskMaster", "id");
+  public static final QueryField TITLE = field("TaskMaster", "title");
+  public static final QueryField BODY = field("TaskMaster", "body");
+  public static final QueryField STATE = field("TaskMaster", "state");
+  private final @ModelField(targetType="ID", isRequired = true) String id;
+  private final @ModelField(targetType="String", isRequired = true) String title;
+  private final @ModelField(targetType="String") String body;
+  private final @ModelField(targetType="String") String state;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
+  public String getId() {
+      return id;
+  }
+  
+  public String getTitle() {
+      return title;
+  }
+  
+  public String getBody() {
+      return body;
+  }
+  
+  public String getState() {
+      return state;
+  }
+  
+  public Temporal.DateTime getCreatedAt() {
+      return createdAt;
+  }
+  
+  public Temporal.DateTime getUpdatedAt() {
+      return updatedAt;
+  }
+  
+  private TaskMaster(String id, String title, String body, String state) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.state = state;
+  }
+  
+  @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
+      TaskMaster taskMaster = (TaskMaster) obj;
+      return ObjectsCompat.equals(getId(), taskMaster.getId()) &&
+              ObjectsCompat.equals(getTitle(), taskMaster.getTitle()) &&
+              ObjectsCompat.equals(getBody(), taskMaster.getBody()) &&
+              ObjectsCompat.equals(getState(), taskMaster.getState()) &&
+              ObjectsCompat.equals(getCreatedAt(), taskMaster.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), taskMaster.getUpdatedAt());
+      }
+  }
+  
+  @Override
+   public int hashCode() {
+    return new StringBuilder()
+      .append(getId())
+      .append(getTitle())
+      .append(getBody())
+      .append(getState())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
+  }
+  
+  @Override
+   public String toString() {
+    return new StringBuilder()
+      .append("TaskMaster {")
+      .append("id=" + String.valueOf(getId()) + ", ")
+      .append("title=" + String.valueOf(getTitle()) + ", ")
+      .append("body=" + String.valueOf(getBody()) + ", ")
+      .append("state=" + String.valueOf(getState()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
+  }
+  
+  public static TitleStep builder() {
+      return new Builder();
+  }
+  
+  /** 
+   * WARNING: This method should not be used to build an instance of this object for a CREATE mutation.
+   * This is a convenience method to return an instance of the object with only its ID populated
+   * to be used in the context of a parameter in a delete mutation or referencing a foreign key
+   * in a relationship.
+   * @param id the id of the existing item this instance will represent
+   * @return an instance of this model with only ID populated
+   * @throws IllegalArgumentException Checks that ID is in the proper format
+   */
+  public static TaskMaster justId(String id) {
+    try {
+      UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
+    } catch (Exception exception) {
+      throw new IllegalArgumentException(
+              "Model IDs must be unique in the format of UUID. This method is for creating instances " +
+              "of an existing object with only its ID field for sending as a mutation parameter. When " +
+              "creating a new object, use the standard builder method and leave the ID field blank."
+      );
+    }
+    return new TaskMaster(
+      id,
+      null,
+      null,
+      null
+    );
+  }
+  
+  public CopyOfBuilder copyOfBuilder() {
+    return new CopyOfBuilder(id,
+      title,
+      body,
+      state);
+  }
+  public interface TitleStep {
+    BuildStep title(String title);
+  }
+  
+
+  public interface BuildStep {
+    TaskMaster build();
+    BuildStep id(String id) throws IllegalArgumentException;
+    BuildStep body(String body);
+    BuildStep state(String state);
+  }
+  
+
+  public static class Builder implements TitleStep, BuildStep {
+    private String id;
+    private String title;
+    private String body;
+    private String state;
+    @Override
+     public TaskMaster build() {
+        String id = this.id != null ? this.id : UUID.randomUUID().toString();
+        
+        return new TaskMaster(
+          id,
+          title,
+          body,
+          state);
+    }
+    
+    @Override
+     public BuildStep title(String title) {
+        Objects.requireNonNull(title);
+        this.title = title;
+        return this;
+    }
+    
+    @Override
+     public BuildStep body(String body) {
+        this.body = body;
+        return this;
+    }
+    
+    @Override
+     public BuildStep state(String state) {
+        this.state = state;
+        return this;
+    }
+    
+    /** 
+     * @param id id
+     * @return Current Builder instance, for fluent method chaining
+     */
+    public BuildStep id(String id) {
+        this.id = id;
+        return this;
+    }
+  }
+  
+
+  public final class CopyOfBuilder extends Builder {
+    private CopyOfBuilder(String id, String title, String body, String state) {
+      super.id(id);
+      super.title(title)
+        .body(body)
+        .state(state);
+    }
+    
+    @Override
+     public CopyOfBuilder title(String title) {
+      return (CopyOfBuilder) super.title(title);
+    }
+    
+    @Override
+     public CopyOfBuilder body(String body) {
+      return (CopyOfBuilder) super.body(body);
+    }
+    
+    @Override
+     public CopyOfBuilder state(String state) {
+      return (CopyOfBuilder) super.state(state);
+    }
+  }
+  
+}
--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.TaskMaster;
 
 public class AddTask extends AppCompatActivity {
 
@@ -17,8 +22,8 @@ public class AddTask extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
 
 
-        AppDatabase database =  Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "shatha").allowMainThreadQueries().build();
-        TaskDao taskDao = database.taskDao();
+//        AppDatabase database =  Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "shatha").allowMainThreadQueries().build();
+//        TaskDao taskDao = database.taskDao();
 
         Button addTasks= (Button) findViewById(R.id.button);
         addTasks.setOnClickListener(new View.OnClickListener() {
@@ -29,9 +34,28 @@ public class AddTask extends AppCompatActivity {
                 EditText taskTitle= findViewById(R.id.taskTitleView);
                 EditText taskDescription= findViewById(R.id.taskDescriptionView);
                 EditText taskState= findViewById(R.id.taskStateView);
+//
+//                 Task task = new Task(taskTitle.getText().toString(),taskDescription.getText().toString(),taskState.getText().toString());
+//                 taskDao.insertAll(task);
 
-                 Task task = new Task(taskTitle.getText().toString(),taskDescription.getText().toString(),taskState.getText().toString());
-                 taskDao.insertAll(task);
+
+
+                TaskMaster todo = TaskMaster.builder()
+                        .title(taskTitle.getText().toString())
+                        .body(taskDescription.getText().toString())
+                        .state(taskState.getText().toString())
+                        .build();
+
+                Amplify.API.mutate(
+                        ModelMutation.create(todo),
+                        response -> Log.i("MyAmplifyApp", "Added Todo with id: " + response.getData().getId()),
+                        error -> Log.e("MyAmplifyApp", "Create failed", error));
+
+
+
+
+
+
             }
         });
 
